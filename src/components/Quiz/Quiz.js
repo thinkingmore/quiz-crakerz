61cedba44f4edf68c297b829d3b8fccae3332d74
thinkingmore <peterkevin823@gmail.comm>
2022-10-11 13:51:54 +0600

    added useState hook and some function to check answers

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import { faBook, faEye, faLockOpen } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import './Quiz.css';
 
 const Quiz = (qs) => {
-    const { key, options, question, correct } = qs;
+    const { options, question, correct } = qs;
+    const [answer,setAnswer] = useState();
+   
+    const checkAnswer = () => {
+    if(answer === correct){    
+        alert("Correct")
+    }
+    else{
+        alert("Incorrect")
+    }
+   }
+
+   
     return (
         <div>
             <section className='quiz'>
-                <h3>{question}</h3> 
-                <input type="radio" name="quizAnswer"/>{options[0]}
+                <h3>{question}<span className='ans-icon'><FontAwesomeIcon icon={faBook}></FontAwesomeIcon></span></h3> 
+                <input type="radio" onChange={e=> setAnswer(e.target.value)} value={options[0]} name="quizAnswer"/>{options[0]}
                 <br />
-                <input type="radio" name="quizAnswer"/>{options[1]}
+                <input type="radio" value={options[1]} onChange={e=> setAnswer(e.target.value)} name="quizAnswer"/>{options[1]}
                 <br />
-                <input type="radio" name="quizAnswer"/>{options[2]}
+                <input type="radio" value={options[2]} onChange={e=> setAnswer(e.target.value)} name="quizAnswer"/>{options[2]}
                 <br />
-                <input type="radio" name="quizAnswer"/>{options[3] }
+                <input type="radio" value={options[3]} onChange={e=> setAnswer(e.target.value)} name="quizAnswer"/>{options[3] }
                 <br />
-                <button className='ans-submit-btn' type="button" onclick="displayRadioValue()">
+                <button onClick={()=> checkAnswer(answer)} className='ans-submit-btn' type="button" onclick="displayRadioValue()">
                     Submit
                 </button>
             </section>
