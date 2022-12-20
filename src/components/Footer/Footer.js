9ce2217288c0685729084a36a5f2bbc0755b78bd
thinkingmore <peterkevin823@gmail.comm>
2022-12-20 11:07:40 +0600

    added a footer section

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <div>
+                <p> Copyright <span>© 2022</span> Skill Test </p>
+            </div>
+        </footer>
+        
+    );
+};
+
+export default Footer;
